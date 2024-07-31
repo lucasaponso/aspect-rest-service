a81619accec5c33cffc7efef70ccd9ea17f7dc34
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-07-31 13:05:37 +1000

    mod: added return of object in get inventory, as well as the status code.

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,9 @@
 from fastapi import Response
-from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter, HTTPException, status
 from deps import validate_database_creds
 import sqlite3
+import bcrypt
 from schemas import UserAuth, SuccessLogin, FailLogin
 from utils import (
     create_access_token,
@@ -12,15 +14,21 @@ router = APIRouter(
 )
 
 @router.post('/request', tags=["user"], summary="Create access and refresh tokens for user")
-async def login(user_creds: UserAuth, response: Response):
+async def login(user_creds: UserAuth):
     if validate_database_creds(user_creds):
+
         access_token = create_access_token(user_creds.username)
+
         conn = sqlite3.connect('db/user.db')
         cursor = conn.cursor()
         cursor.execute('''INSERT INTO user (username, password) VALUES (?, ?)''', (user_creds.username,user_creds.password,))
         conn.commit()
         conn.close()
 
-        return SuccessLogin(username=user_creds.username, token=access_token, status=True, msg="User has been login successfully")
+        login_attempt = SuccessLogin(username=user_creds.username, token=access_token, status=True, msg="User has been login successfully")
+        status_code = status.HTTP_200_OK
     else:
-        return FailLogin(username=user_creds.username, status=False, msg=f"Incorrect login credentials, thw following user was unable to login successfully: {user_creds.username}")
+        status_code = status.HTTP_401_UNAUTHORIZED
+        login_attempt = FailLogin(username=user_creds.username, status=False, msg=f"Incorrect login credentials, thw following user was unable to login successfully: {user_creds.username}")
+
+    return JSONResponse(status_code=status_code, content=login_attempt.dict())
