54f195019fa89c306d0eb6fcb6957c5d06d07f07
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-07-31 10:49:14 +1000

    mod: modified JSON response to match required JSON response

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-# How to build docker container
+# ASPECT REST Service
+The following repository hosts the software belonging to the ASPECT REST service. This API is accessed to retrieve data from the ASPECT server.
+
+## API Runtime Environment
+This software runs in a Docker container.
+A docker container is a small virtual machine that has the minimal tools and applications needed to execute the API. This docker container comes with it's own operating system, by default the docker container uses the Ubuntu Linux Distribution. The instructions that are given to docker to start a container is within the Dockerfile.
+
+
+# How to build and run ASPECT REST Service
+
+## Windows
+#### Install WSL
+Navigate to the Windows command prompt (run as administrator).
+
+//PHOTO
+
+Run the following command: ```wsl --install ```
+
+//PHOTO 
+
+Once WSL has been installed you may need to restart the Windows machine.
+
+//PHOTO
+
+
+#### Install Docker Desktop
+After WSL has been installed, navigate to a web browser and search for Docker Desktop and install the Windows Version.
+
+//PHOTO
+
+Click on the executable and follow the setup wizard.
+
+//PHOTO
+
+Once everything has installed, you will most likely need to restart the machine.
+
+//PHOTO
+
+
+#### Pull the Docker Container from DockerHub
+After the Docker Desktop has installed, then
+
 To build the following docker container run the following command:
 
 ``` sudo docker build -t avara2024/aspect-rest-service:latest .```
--- routes/data.py
+++ routes/data.py
@@ -1,61 +1,10 @@
-# from fastapi import Depends, APIRouter
-# from sqlalchemy.orm import Session
-# from db.init import get_db
-# from typing import Annotated
-# from pydantic import BaseModel
-
-# router = APIRouter(
-#     prefix="/core"
-# )
-# class DeviceInfo(BaseModel):
-#     serialNo: str
-#     unitSerialNo: str
-#     unitName: str
-#     productType: str
-#     productCode: str
-#     hwVersion: str
-#     fpgaVersion: str
-#     bootLoader: str
-#     activeSwVersion: str
-#     backupSwVersion: str
-#     assetNo: str
-#     ipAddress: str
-
-# db_dependency = Annotated[Session, Depends(get_db)]
-
-# @router.get('/inventory',  tags=["data"], summary='Retrieves all data associated with the device_info_tab table')
-# async def getInventory(db: db_dependency):
-#     cursor = db.cursor()
-#     cursor.execute('SELECT serial_no, unit_serial_no, unit_name, product_type, product_code, hw_version, fpga_version, boot_loader, active_sw_version, backup_sw_version, asset_no, ip_address FROM device_info_tab FOR JSON PATH;')
-#     rows = cursor.fetchall()
-
-#     device_info_list = [
-#         DeviceInfo(
-#             serialNo=row[0],
-#             unitSerialNo=row[1],
-#             unitName=row[2],
-#             productType=row[3],
-#             productCode=row[4],
-#             hwVersion=row[5],
-#             fpgaVersion=row[6],
-#             bootLoader=row[7],
-#             activeSwVersion=row[8],
-#             backupSwVersion=row[9],
-#             assetNo=row[10],
-#             ipAddress=row[11]
-#         )
-#         for row in rows
-#     ]
-#     return device_info_list
-
-
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from db.init import get_db
 from typing import Annotated, List
 from pydantic import BaseModel
-import datetime
+from datetime import datetime
 
 router = APIRouter(prefix="/core")
 
@@ -63,24 +12,37 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 # Define a Pydantic model for the response
 class DeviceInfo(BaseModel):
-    serial_no: str = Query(max_length=32)
+    linked_key: str = Query(max_length=32)
     unit_serial_no: str = Query(max_length=32)
     unit_name: str = Query(max_length=100)
-    # product_type: str = Query(max_length=50)
-    # product_code: str = Query(max_length=50)
-    # hw_version: str = Query(max_length=50)
-    # fpga_version: str = Query(max_length=50)
-    # boot_loader: str = Query(max_length=50)
-    # active_sw_version: str = Query(max_length=50)
-    # backup_sw_version: str = Query(max_length=50)
-    # asset_no: str = Query(max_length=50)
-    # ip_address: str = Query(max_length=15)  ##perhaps change this to a ip address type
+    product_type: str = Query(max_length=50)
+    product_code: str = Query(max_length=50)
+    hw_version: str = Query(max_length=50)
+    fpga_version: str = Query(max_length=50)
+    boot_loader: str = Query(max_length=50)
+    active_sw_version: str = Query(max_length=50)
+    backup_sw_version: str = Query(max_length=50)
+    asset_no: str = Query(max_length=50)
+    ip_address: str = Query(max_length=15)  ##perhaps change this to a ip address type
+    slot_id: int ##set max and min here
+    location: str = Query(max_length=100)
+    config_signature : str = Query(max_length=50)
+    companyname : str = Query(max_length=50)
+    mac_address: str = Query(max_length=17)
+    imei: str = Query(max_length=50)
+    imsi: str = Query(max_length=50)
+    iccid: str = Query(max_length=50)
+    phone_no : str = Query(max_length=50)
+    phone_no_2 : str = Query(max_length=50)
+    imsi_2: str = Query(max_length=50)
+    iccid_2: str = Query(max_length=50)
+    config_name: str = Query(max_length=100)
+    nav_submap_id: str = Query(max_length=50)
+
     # unit_password: str = Query(max_length=100)
-    # slot_id: int ##set max and min here
-    # last_access: str
+
     # child_node: str = Query(max_length=5)
     # xtab_mode: str = Query(max_length=15)
-    # location: str = Query(max_length=100)
     # config_signature: str = Query(max_length=50)
     # node_state: str = Query(max_length=10)
     # region_id: int ##set max and min here
@@ -92,7 +54,6 @@ class DeviceInfo(BaseModel):
     # snmask : str = Query(max_length=15) ##perhaps change this to a ip address type
     # gwaddr : str = Query(max_length=15) ##perhaps change this to a ip address type
     # csaddr : str = Query(max_length=50)
-    # companyname : str = Query(max_length=50)
     # community_name : str = Query(max_length=50)
     # discovered_node : str = Query(max_length=5)
     # user_name : str = Query(max_length=50)
@@ -109,20 +70,11 @@ class DeviceInfo(BaseModel):
     # default_app : str = Query(max_length=20)
     # bp_mode : str = Query(max_length=20)
     # backplain_bus : int ##set max and min here
-    # discovered_date: str
-    # mac_address: str = Query(max_length=17)
     # subnet_id : int ##set max and min here
-    # imei: str = Query(max_length=50)
-    # imsi: str = Query(max_length=50)
-    # iccid: str = Query(max_length=50)
     # phone_no: str = Query(max_length=50)
     # phone_no_2: str = Query(max_length=50)
     # use_same_ip: str = Query(max_length=5)
     # enable_config_save: str = Query(max_length=5)
-    # nav_submap_id: str = Query(max_length=50)
-    # imsi_2: str = Query(max_length=50)
-    # iccid_2: str = Query(max_length=50)
-    # config_name: str = Query(max_length=100)
     # template_1: str = Query(max_length=200)
     # template_2: str = Query(max_length=200)
     # template_3: str = Query(max_length=200)
@@ -144,7 +96,30 @@ async def get_inventory(db: db_dependency):
     query = """SELECT
             serial_no,
             unit_serial_no,
-            unit_name
+            unit_name,
+            product_type,
+            product_code,
+            hw_version,
+            fpga_version,
+            boot_loader,
+            active_sw_version,
+            backup_sw_version,
+            asset_no,
+            ip_address,
+            slot_id,
+            location,
+            config_signature,
+            companyname,
+            mac_address,
+            imei,
+            imsi,
+            iccid,
+            phone_no,
+            phone_no_2,
+            imsi_2,
+            iccid_2,
+            config_name,
+            nav_submap_id
             FROM device_info_tab """
 
     cursor = db.cursor()
@@ -155,9 +130,32 @@ async def get_inventory(db: db_dependency):
     device_info_list = []
     for row in rows:
         device_info = DeviceInfo(
-            serial_no=row[0],
+            linked_key=row[0],
             unit_serial_no=row[1],
-            unit_name=row[2]
+            unit_name=row[2],
+            product_type=row[3],
+            product_code=row[4],
+            hw_version=row[5],
+            fpga_version=row[6],
+            boot_loader=row[7],
+            active_sw_version=row[8],
+            backup_sw_version=row[9],
+            asset_no=row[10],
+            ip_address=row[11],
+            slot_id=row[12],
+            location=row[13],
+            config_signature=row[14],
+            companyname=row[15],
+            mac_address=row[16],
+            imei=row[17],
+            imsi=row[18],
+            iccid=row[19],
+            phone_no=row[20],
+            phone_no_2=row[21],
+            imsi_2=row[22],
+            iccid_2=row[23],
+            config_name=row[24],
+            nav_submap_id=row[25]
             )
         device_info_list.append(device_info)
 
