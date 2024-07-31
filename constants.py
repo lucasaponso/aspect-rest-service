eccc12866823b830814ef673b0c097a1fcdbc814
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-07-31 12:23:37 +1000

    mod: removed commented env vars

--- constants.py
+++ constants.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 """
 The following file (constants.py) loads the environment variables that were set in the Dockerfile. See the Dockerfile for more information.
 We use the os library to access the value of the env variable. The remaining project files import these variables.
@@ -9,9 +13,3 @@ ASPECT_DB = os.getenv('ASPECT_DB')
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
 ALGORITHM = os.getenv('ALGORITHM')
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-
-# ASPECT_SERVER_HOST = "10.10.110.95"
-# ASPECT_DB = "ASPECT_DB"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 30
-# ALGORITHM = "HS256"
-# JWT_SECRET_KEY = "ef3ffea74db9ba1606ecbe0c68261b87e6f9b0c1b90e406b136a3b1423e4fcb7"
